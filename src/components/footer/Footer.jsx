04dde38b5404e8f53d52b5985306a41cf2506d78
Hernán Pablo Lijó <lijo.hernanpablo@gmail.com>
2024-03-29 19:50:07 -0300

    texto footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,6 +7,7 @@ const header = () => {
         <footer className={classes.footer}>
             <div className={classes.footer__title}>
                 <h3 className={classes.footer__h3}>App creada por Hernán Lijó</h3>            
+                <h4 className={classes.footer__h2}>Los productos son de muestra, no es una venta real</h4>            
             </div>
         </footer>
     );
