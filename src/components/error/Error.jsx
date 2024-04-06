15c7ebbb70a2ebcc2e018de3f28a4f22542e2c2c
Hernán Pablo Lijó <lijo.hernanpablo@gmail.com>
2024-04-05 23:24:14 -0300

    ruta error

--- src/components/error/Error.jsx
+++ src/components/error/Error.jsx
@@ -5,7 +5,8 @@ const Error = () => {
     return (
         <div className='error__cont'>
             <h2 className='error__titulo'>Ups... Nada por aqui</h2>
-            <img src="../../public/data/error.gif" alt="ERROR" className='error_img' />
+            <img src='https://firebasestorage.googleapis.com/v0/b/ecomerce2024-7e06d.appspot.com/o/error.gif?alt=media&token=0cb613ef-af0b-4ef0-b57a-b11d6d8ea7f9' 
+            alt="ERROR" className='error_img' />
         </div>
     );
 };
